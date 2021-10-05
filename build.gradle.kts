56036da8904006272bf6eb5256ac16090383f4a2
D. Lowl <d.lowl@d-lowl.space>
2021-10-05 20:58:02 +0500

    Add publishing configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,9 @@ plugins {
 group = "space.dlowl"
 version = "1.1-SNAPSHOT"
 
+val spaceUsername: String by project
+val spacePassword: String by project
+
 repositories {
     mavenCentral()
 }
@@ -28,6 +31,33 @@ kotlin {
         else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
     }
 
+    val publicationsFromMainHost =
+        listOf(jvm()).map { it.name } + "kotlinMultiplatform"
+
+    publishing {
+        publications {
+            create<MavenPublication>("maven") {
+                groupId = "space.dlowl"
+                artifactId = "kmenu"
+                version = version
+                matching { it.name in publicationsFromMainHost }.all {
+                    val targetPublication = this@all
+                    tasks.withType<AbstractPublishToMaven>()
+                        .matching { it.publication == targetPublication }
+                        .configureEach { onlyIf { findProperty("isMainHost") == "true" } }
+                }
+            }
+        }
+        repositories {
+            maven {
+                url = uri("https://maven.pkg.jetbrains.space/dlowl/p/kmenu/kmenu-maven")
+                credentials {
+                    username = spaceUsername
+                    password = spacePassword
+                }
+            }
+        }
+    }
     
     sourceSets {
         val commonMain by getting
@@ -42,3 +72,5 @@ kotlin {
         val nativeTest by getting
     }
 }
+
+
